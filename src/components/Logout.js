0a1c0c545ec4a1dbe8051924354f4c5355592aca
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-12 11:41:39 -0800

    Logout route now logs user out

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,8 +1,27 @@
-import React from 'react';
+// Libraries
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Logout = () => {        
-    return(<div></div>);
-}
+// Axios with Authentication
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const Logout = () => {
+  const { push } = useHistory();
+
+  useEffect(() => {
+    axiosWithAuth()
+      .post(`http://localhost:5000/api/logout`)
+      .then(() => {
+        localStorage.removeItem("token");
+        push("/login");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (<div></div>);
+};
 
 export default Logout;
 
